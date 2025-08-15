9f08b8ba15ea6043046feabf22b16060102e5b12
Lunarmist-byte <amal.s.kuamr2005@gmail.com>
2025-08-15 22:05:50 +0530

    changed pass1 to my code

--- pass1.c
+++ pass1.c
@@ -1,105 +1,59 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAX_SYM 50
 #define BUF 20
 
-typedef struct symtab {
-	char lab[20];
-	int addr;
-} stab;
-
-typedef struct optab {
-	char opnd[20];
-	int addr;
-} optb;
-
-int st_cnt = 0, otsize = 0, start = 0, locctr = 0;
-stab st[MAX_SYM];
-optb otab[MAX_SYM];
-
-void run (FILE* inter, char opcode[], char label[], char op[]) {
-	if (strcmp(opcode, "START") == 0) {
-		start = locctr = (int) strtol(op, NULL, 16);
-		fprintf(inter, "%X\t%s\t%s\t%s\n", start, label, opcode, op);
-		return;
-	}
-	int inc;
-	if (strcmp(label, "**")) {
-		int found = 0;
-		for (int i = 0; i < st_cnt; i++) {
-			if (!strcmp(st[i].lab, label)) {
-				found = 1;
-			}
-		}
-		if (found) {
-			printf("Symbol %s Already Declared!!!\n");
-			exit(1);
-		} else {
-			strcpy(st[st_cnt].lab, label);
-			st[st_cnt++].addr = locctr;
-		}
-	}
-	if (!strcmp(opcode, "BYTE"))
-		inc = strlen(op);
-	else if (!strcmp(opcode, "WORD"))
-		inc = 3;
-	else if (!strcmp(opcode, "RESB"))
-		inc = (int) strtol(op, NULL, 10);
-	else if(!strcmp(opcode, "RESW"))
-		inc = 3 * (int) strtol(op, NULL, 10);
-	else {
-		int found = 0;
-		for (int i = 0; i < otsize; i++) {
-			if (!strcmp(opcode, otab[i].opnd)) {
-				found = 1;
-				break;
-			}
-		}
-		if (found)
-			inc = 3;
-		else if (!strcmp(opcode, "END"))
-			inc = 0;
-		else {
-			printf("OPCODE %s NOT FOUND!!!\n", opcode);
-			exit(1);
-		}
-	}
-	fprintf(inter, "%X\t%s\t%s\t%s\n", locctr, label, opcode, op);
-	locctr += inc;
-}
-
+typedef struct { char lab[20]; int addr; } stab;
+typedef struct { char opnd[20]; int len; } optb;
 
 int main() {
-	FILE* src = fopen("input.sic", "r");
-	FILE* optab = fopen("optab", "r");
-	FILE* inter = fopen("intermediate.sic", "w");
-	FILE* symtab = fopen("symtab", "w");
+    FILE *src=fopen("source.txt","r");
+    FILE *symtab=fopen("symtab","w");
+    FILE *optab=fopen("optab","r");
+    FILE *inter=fopen("intermediate.txt","w");
+    if(!src||!symtab||!optab||!inter){ printf("File error\n"); return 1; }
 
-	if (!src || !optab || !inter || !symtab) {
-		printf("Couldnot Open File...\n");
-		return 0;
-	}
+    optb ot[MAX_SYM];
+    int ot_cnt=0;
+    while(fscanf(optab,"%s%d",ot[ot_cnt].opnd,&ot[ot_cnt].len)==2) ot_cnt++;
 
-	char label[BUF], opcode[BUF], op[BUF];
-	int opint;
-	while (fscanf(optab, "%s\t%d", opcode, &opint) > 0) {
-		strcpy(otab[otsize].opnd, opcode);
-		otab[otsize++].addr = opint;
-	}				// to fetch optab into memory...
+    int start_addr=0,locctr=0;
+    char label[BUF],opcode[BUF],operand[BUF];
+    fscanf(src,"%s%s%s",label,opcode,operand);
 
-	printf("Implementing Pass 1...\nIntermediate File...\n");
-	printf("%10s%10s%10s%10s\n", "ADDRESS", "LABEL", "OPCODE", "OPERAND");
-	do {
-		fscanf(src, "%s\t%s\t%s", &label, &opcode, &op);
-		printf("%10X%10s%10s%10s\n", locctr, label, opcode, op);
-		run(inter, opcode, label, op);
-	} while (strcmp(opcode, "END"));
-	for (int i = 0; i < st_cnt; i++) {
-		fprintf(symtab, "%s\t%X\n", st[i].lab, st[i].addr);
-	}
-	printf("Intermediate file saved as \"intermediate.sic\"\n");
-	printf("SYMTAB saved as \"symtab\"\n");
-	return 0;
+    if(strcmp(opcode,"START")==0) {
+        start_addr=strtol(operand,NULL,16);
+        locctr=start_addr;
+        fprintf(inter,"%04X %s %s %s\n",locctr,label,opcode,operand);
+        fscanf(src,"%s%s%s",label,opcode,operand);
+    }
+
+    while(strcmp(opcode,"END")!=0) {
+        fprintf(inter,"%04X %s %s %s\n",locctr,label,opcode,operand);
+        if(strcmp(label,"-")!=0) fprintf(symtab,"%s %04X\n",label,locctr);
+
+        int found=0;
+        for(int i=0;i<ot_cnt;i++) {
+            if(strcmp(opcode,ot[i].opnd)==0) { locctr+=ot[i].len; found=1; break; }
+        }
+
+        if(!found) {
+            if(strcmp(opcode,"WORD")==0) locctr+=3;
+            else if(strcmp(opcode,"RESW")==0) locctr+=3*atoi(operand);
+            else if(strcmp(opcode,"RESB")==0) locctr+=atoi(operand);
+            else if(strcmp(opcode,"BYTE")==0) {
+                if(operand[0]=='C') locctr+=strlen(operand)-3;
+                else if(operand[0]=='X') locctr+=(strlen(operand)-3)/2;
+            } else printf("Error: Invalid opcode %s\n",opcode);
+        }
+
+        fscanf(src,"%s%s%s",label,opcode,operand);
+    }
+    fprintf(inter,"    %s %s %s\n",label,opcode,operand);
+
+    fclose(src); fclose(symtab); fclose(optab); fclose(inter);
+    printf("Pass 1 complete.\n");
+    return 0;
 }
